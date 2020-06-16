03fec8e967d729620a86697899577a012eda76cd
R_XIAN <asdzxccxz333@gmail.com>
2020-06-16 18:39:26 +0800

    connect to heroku

--- src/php/connectMysql.php
+++ src/php/connectMysql.php
@@ -3,10 +3,10 @@ header('Access-Control-Allow-Origin: *');
 header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
 header('Access-Control-Allow-Methods: GET, POST, PUT,DELETE');
 
-$db_host = "localhost";
-$db_username = "root";
-$db_password = "";
-$db_name = "longping";
+$db_host = "us-cdbr-east-05.cleardb.net";
+$db_username = "b2bdf39ee7a7c4";
+$db_password = "550f3665";
+$db_name = "heroku_0becba99ffc63e8";
 $method = $_SERVER['REQUEST_METHOD'];
 $db_link = @new mysqli($db_host, $db_username, $db_password, $db_name);
 if ($db_link->connect_error != "") {
