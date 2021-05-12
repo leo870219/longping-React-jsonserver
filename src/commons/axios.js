afa3fc7fae86f8ae8dfd377b09eaa27158ec1200
R_XIAN <asdzxccxz333@gmail.com>
2021-05-13 02:27:35 +0800

    接回遠端資料庫

--- src/commons/axios.js
+++ src/commons/axios.js
@@ -1,7 +1,7 @@
 import _axios from "axios";
 const axios = (baseURL) => {
   const instance = _axios.create({
-    baseURL: baseURL || "http://localhost:3003/", //"https://longping-jsonserver-api.herokuapp.com/",
+    baseURL: baseURL || "https://longping-jsonserver-api.herokuapp.com/",
     timeout: 6000,
   });
 
